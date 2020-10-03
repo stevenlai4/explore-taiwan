cba1fd296ea923732cb3bae01492fdf200569229
Steven <clai86@my.bcit.ca>
2020-10-02 18:27:43 -0700

    attration page finished

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Hamburger Menu Toggle
 var toggle = document.querySelector('.toggle');
 var menu = document.querySelector('.menu');
 
@@ -15,3 +16,30 @@ function toggleMenu() {
 }
 
 toggle.addEventListener('click', toggleMenu);
+
+// Slide Animation
+const sliders = document.querySelectorAll('.slide-in');
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: '0px 0px -200px 0px',
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+    entries,
+    appearOnScroll
+) {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+},
+appearOptions);
+
+sliders.forEach((slider) => {
+    appearOnScroll.observe(slider);
+});
